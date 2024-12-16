93282b2c5cf54771fefbbf356ac6888e3aa7b8a1
lyoe404 <168478085+lyoe404@users.noreply.github.com>
2024-12-16 20:09:12 +0300

    Delete ayrık_yapılar/text_parser.js

--- ayrık_yapılar/text_parser.js
+++
@@ -1,6 +0,0 @@
- function textParser(text) {
-  const letters = text.split("").filter(char => /[a-zA-ZığüşöçİĞÜŞÖÇ]/.test(char));;
-  console.log(letters[2]) ; 
-  return letters;
-}
-module.exports = {textParser} ; 
